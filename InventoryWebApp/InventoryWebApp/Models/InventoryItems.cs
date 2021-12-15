406b82bd7d6a2181d3b0463e39c44576cf9e6383
shea cooke <SheaSCooke@gmail.com>
2021-12-15 13:36:08 -0500

    initial commit

---
+++ InventoryWebApp/InventoryWebApp/Models/InventoryItems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryWebApp.Models
+{
+    public class InventoryItems
+    {
+        public class InventoryItem
+        {
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public int Quantity { get; set; }
+
+            public double PricePerItem { get; set; }
+
+
+            public InventoryItem(string name, string description, int quantity, double pricePerItem)
+            {
+                this.Name = name;
+                this.Description = description;
+                this.Quantity = quantity;
+                this.PricePerItem = pricePerItem;
+            }
+
+
+
+        }
+    }
+}
---
+++ InventoryWebApp/InventoryWebApp/ViewModel/InventoryItemsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryWebApp.ViewModel
+{
+    public class InventoryItemsViewModel
+    {
+        public class InventoryItemViewModel
+        {
+            [Required(ErrorMessage = "This property is required")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "This property is required")]
+            public string Description { get; set; }
+
+            [Required(ErrorMessage = "This property is required")]
+            public int Quantity { get; set; }
+
+            [Required(ErrorMessage = "This property is required")]
+            public double PricePerItem { get; set; }
+        }
+    }
+}
